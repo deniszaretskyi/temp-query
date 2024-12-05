31ee91aa8b109fee22f0d263934ff5ed889865dd
Denys Zaretskyi <denis.zaretskyi@icloud.com>
2024-12-05 20:42:41 +0200

    Add Funtionality

--- front/src/SingleItem.jsx
+++ front/src/SingleItem.jsx
@@ -1,10 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import customFetch from "./utils";
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+  const { mutate: changeTask } = useMutation({
+    mutationKey: ["tasks"],
+    mutationFn: (id) => customFetch.patch(`/${id}`, { isDone: !item.isDone }),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      toast.success("Task was changed");
+    },
+    onError: () => {
+      toast.error("Error while changing");
+    },
+  });
+  const { mutate: deleteTask } = useMutation({
+    mutationKey: ["tasks"],
+    mutationFn: (id) => customFetch.delete(`/${id}`),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+      toast.success("Task was deleted");
+    },
+    onError: () => {
+      toast.error("Error while deleting");
+    },
+  });
+
   return (
     <div className="single-item">
       <input
         type="checkbox"
         checked={item.isDone}
-        onChange={() => console.log("edit task")}
+        onChange={() => {
+          changeTask(item.id);
+        }}
       />
       <p
         style={{
@@ -17,7 +46,7 @@ const SingleItem = ({ item }) => {
       <button
         className="btn remove-btn"
         type="button"
-        onClick={() => console.log("delete task")}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
